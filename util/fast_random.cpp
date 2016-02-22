ca3facb8eeb02d2619af61b48b210f9121efadf0
madewithlinux <madewithlinux.josh@gmail.com>
2016-02-21 21:55:06 -0600

    improved fast_random

--- util/fast_random.cpp
+++ util/fast_random.cpp
@@ -4,38 +4,41 @@
  * NOT FOR CRYPTOGRAPHIC USES, THIS IS NOT NEARLY RANDOM ENOUGH FOR THAT!!
  */
 #include <iostream>
-#include <unistd.h>
-#include <stdio.h>
+#include <unistd.h> /*isatty, fileno, stdout*/
 #include <ctime>
 #include <random>
+#include <algorithm>
 
-const std::size_t BUFFER_SIZE = 1024 * 1024;
+/*valgrind doesn't like the compiiled result of std::random_device*/
+//#define VALGRIND_COMPATIBLE 1
 
-unsigned long long random_ullong() {
-    return ((unsigned long long) rand()) |
-           (((unsigned long long) rand()) << 32);
-}
+const std::size_t buffer_size = 1024 * 10; /*10KB buffer*/
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char* argv[]) {
+    /* don't want to write random garbage to a terminal because that hurts the
+     * terminal */
     if (isatty(fileno(stdout))) {
         std::cout << "Will not write output to terminal!" << std::endl;
         std::cout << "usage: " << argv[0] << " > [output file]" << std::endl;
         std::cout << "usage: " << argv[0] << " | COMMAND" << std::endl;
         return 1;
     }
-    srand((unsigned int) time(nullptr));
-    unsigned long long *randoms = new unsigned long long int[BUFFER_SIZE];
-    for (int i=0; i<BUFFER_SIZE; i++) {
-        randoms[i] = random_ullong();
-    }
+    std::mt19937_64 prng;
+#ifdef VALGRIND_COMPATIBLE
+    prng.seed(time(NULL));
+#else
+    std::random_device d;
+    prng.seed(d());
+#endif
+    /*make a buffer to store random data*/
+    std::mt19937_64::result_type *out = new std::mt19937_64::result_type[buffer_size];
+    /*as long as cout is still able to be written to*/
     while (!std::cout.eof()) {
-        unsigned long long int newrand = random_ullong();
-        for (int i=0; i<BUFFER_SIZE; i++) {
-            randoms[i] ^= newrand;
-            newrand ^= randoms[BUFFER_SIZE - i];
-        }
-        std::cout.write((char *) randoms, sizeof(unsigned long long) * BUFFER_SIZE);
+        /*fill the buffer with random data*/
+        std::generate(out, out + buffer_size, prng);
+        /*write the buffer*/
+        std::cout.write((char *) out,
+                        sizeof(std::mt19937_64::result_type) * buffer_size);
     }
-
     return 0;
 }
