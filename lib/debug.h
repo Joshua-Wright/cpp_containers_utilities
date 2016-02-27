6f0acbd99ab7b171fe818ccb2c016a57414afc58
madewithlinux <madewithlinux.josh@gmail.com>
2016-02-27 12:31:24 -0600

    added test for debug functions

--- lib/debug.h
+++ lib/debug.h
@@ -1,25 +1,50 @@
 // (c) Copyright 2015 Josh Wright
 #pragma once
 
-#include <iostream>
-#include <cstring> // for strlen()
-#include <libgen.h> // for basename()
+#include <iostream> // for std::cerr and std::endl
+#include <typeinfo> // for typeid(T).name()
+#include <cstring> // for strlen() and basename()
+#include <cxxabi.h> // for abi::__cxa_demangle()
+
+
+/*use this basename if your <string.h> doesn't provide one*/
+//const char* basename(const char* string) {
+//    /*simple basename function which doesn't require dynamically allocating a
+//     * second C String*/
+//    const char *first = string;
+//    const char *last = string + strlen(string);
+//    const char *current = last;
+//    while (current > first) {
+//        if (*(current -1) == '/') {
+//            break;
+//        }
+//        current--;
+//    }
+//    return current;
+//}
 
 template<typename T>
-void debug_log(T var, const char* label, const char *file, int line) {
-    char *new_str = new char[strlen(file) + 1];
-    strcpy(new_str, file);
-    std::cout << basename(new_str) << ":" << line << " " << label << "=" << var << std::endl;
+void __debug_log(T v, const char *l, const char *f, int line, bool p) {
+    /*debug logger that uses template type resolution to print whatever we give it*/
+    std::cerr << basename(f) << ":" << line << " ";
+    if (p) {
+        /*gcc-specific way to demangle the type names, probably not portable*/
+        std::cerr << abi::__cxa_demangle(typeid(T).name(), NULL, NULL, NULL) <<
+        " ";
+    }
+    std::cerr << l << "=" << v << std::endl;
 }
-#define debug_log(x) log(x, #x, __FILE__, __LINE__)
+
+#define debug_log(x) __debug_log(x, #x, __FILE__, __LINE__, true)
+#define debug_log_type(x) __debug_log(x, #x, __FILE__, __LINE__, true)
+#define debug_log_notype(x) __debug_log(x, #x, __FILE__, __LINE__, false)
 
 
-void test(bool expr, const std::string &message, const char *file, int line) {
+void __test(bool expr, const std::string &message, const char *file, int line) {
     if (!expr) {
-        char *new_str = new char[strlen(file) + 1];
-        strcpy(new_str, file);
-        std::cout << basename(new_str) << ":" << line << "  Test failed: " <<
+        std::cerr << basename(file) << ":" << line << "  Test failed: " <<
         message << std::endl;
     }
 }
-#define test(x, y) test(x,y, __FILE__, __LINE__)
+
+#define test(expr, label) __test(expr, label, __FILE__, __LINE__)
---
+++ tests/test_debug.cpp
@@ -0,0 +1,35 @@
+// (c) Copyright 2015 Josh Wright
+
+#include "../lib/debug.h"
+
+int main() {
+    test(true, "This should succeed and not show");
+    test(false, "This outght to fail");
+
+    std::string var = "asdf";
+    int var2 = 1234;
+    debug_log_notype(var);
+    debug_log_type(var);
+    debug_log(&var);
+    debug_log_notype(var2);
+    debug_log_type(var2);
+    debug_log(&var2);
+    debug_log(var.c_str());
+    debug_log(     5);
+    debug_log(5     );
+    debug_log((float) 123.431243213123123213123123123);
+    debug_log((int) 123.431243213123123213123123123);
+    debug_log(123.431243213123123213123123123);
+    debug_log('a');
+    debug_log('as');
+    debug_log('asd');
+    debug_log('asdf');
+    debug_log("asdf");
+    debug_log(__test);
+    debug_log(&__test);
+    debug_log(basename("test"));
+    debug_log(basename("/test"));
+    debug_log(basename("/path/to/test"));
+
+    return 0;
+}
