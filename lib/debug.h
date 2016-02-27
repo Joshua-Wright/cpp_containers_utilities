318343fc688488d0393ef09e47698edfd504c28f
madewithlinux <madewithlinux.josh@gmail.com>
2016-02-27 13:15:44 -0600

    added print_trace()

--- lib/debug.h
+++ lib/debug.h
@@ -6,8 +6,35 @@
 #include <cstring> // for strlen() and basename()
 #include <cxxabi.h> // for abi::__cxa_demangle()
 
+/*for print_trace()*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/prctl.h>
+/*source:
+http://stackoverflow.com/questions/4636456/how-to-get-a-stack-trace-for-c-using-gcc-with-line-number-information/4732119#4732119
+ */
+void print_trace() {
+    char pid_buf[30];
+    sprintf(pid_buf, "%d", getpid());
+    char name_buf[512];
+    name_buf[readlink("/proc/self/exe", name_buf, 511)]=0;
+    prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
+    int child_pid = fork();
+    if (!child_pid) {
+        dup2(2,1); // redirect output to stderr
+        fprintf(stdout,"stack trace for %s pid=%s\n",name_buf,pid_buf);
+        execlp("gdb", "gdb", "--batch", "-n", "-ex", "thread", "-ex", "bt", name_buf, pid_buf, NULL);
+        abort(); /* If gdb failed to start */
+    } else {
+        waitpid(child_pid,NULL,0);
+    }
+}
 
-/*use this basename if your <string.h> doesn't provide one*/
+
+
+/*use this basename() if your <string.h> doesn't provide one*/
 //const char* basename(const char* string) {
 //    /*simple basename function which doesn't require dynamically allocating a
 //     * second C String*/
