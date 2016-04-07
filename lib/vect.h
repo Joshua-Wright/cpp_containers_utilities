225bf75fe7309868eb9d6398aeada2fe013f4276
madewithlinux <madewithlinux.josh@gmail.com>
2016-04-07 18:05:41 -0500

    uses std::transform in more places where possible

--- lib/vect.h
+++ lib/vect.h
@@ -15,6 +15,10 @@ namespace containers {
   template<typename T, size_t dim>
   struct vect : public std::array<T, dim> {
     typedef std::array<T, dim> array;
+    using array::begin;
+    using array::end;
+    using array::cbegin;
+    using array::cend;
   public:
 
     vect() : array() { }
@@ -23,46 +27,41 @@ namespace containers {
       if (list.size() != dim) {
         throw std::logic_error("Bad array initializer size!");
       }
-      std::copy(list.begin(), list.end(), array::begin());
+      std::copy(list.begin(), list.end(), begin());
     }
 
     template<typename U>
     vect<T, dim> &operator+=(const U &rhs) {
-      for (auto &x : *this) {
-        x += rhs;
-      }
+      std::transform(begin(), end(), begin(),
+                     [&rhs](const T &a) { return a + rhs; });
       return *this;
-    }
+    };
 
     template<typename U>
     vect<T, dim> &operator-=(const U &rhs) {
-      for (auto &x : *this) {
-        x -= rhs;
-      }
+      std::transform(begin(), end(), begin(),
+                     [&rhs](const T &a) { return a - rhs; });
       return *this;
     };
 
     template<typename U>
     vect<T, dim> &operator*=(const U &rhs) {
-      for (auto &x : *this) {
-        x *= rhs;
-      }
+      std::transform(begin(), end(), begin(),
+                     [&rhs](const T &a) { return a * rhs; });
       return *this;
     };
 
     template<typename U>
     vect<T, dim> &operator/=(const U &rhs) {
-      for (auto &x : *this) {
-        x /= rhs;
-      }
+      std::transform(begin(), end(), begin(),
+                     [&rhs](const T &a) { return a / rhs; });
       return *this;
     };
 
     template<typename U>
     vect<T, dim> &operator%=(const U &rhs) {
-      for (auto &x : *this) {
-        x %= rhs;
-      }
+      std::transform(begin(), end(), begin(),
+                     [&rhs](const T &a) { return a % rhs; });
       return *this;
     };
 
@@ -112,9 +111,9 @@ namespace containers {
 
   template<typename T, size_t dim, typename U>
   vect<T, dim> operator-(const U &rhs, const vect<T, dim> &lhs) {
-    vect<T, dim> out = lhs;
-    std::transform(out.begin(), out.end(), out.begin(),
-                   [&rhs](T &a) { return rhs - a; });
+    vect<T, dim> out;
+    std::transform(lhs.cbegin(), lhs.cend(), out.begin(),
+                   [&rhs](const T &a) { return rhs - a; });
     return out;
   }
 
@@ -127,9 +126,9 @@ namespace containers {
 
   template<typename T, size_t dim, typename U>
   vect<T, dim> operator/(const U &rhs, const vect<T, dim> &lhs) {
-    vect<T, dim> out = lhs;
-    std::transform(out.begin(), out.end(), out.begin(),
-                   [&rhs](T &a) { return rhs / a; });
+    vect<T, dim> out;
+    std::transform(lhs.begin(), lhs.end(), out.begin(),
+                   [&rhs](const T &a) { return rhs / a; });
     return out;
   }
 
